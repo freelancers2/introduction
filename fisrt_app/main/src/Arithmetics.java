f5b249ce22362324a723fe517da97885c063f4fe
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-18 21:41:40 +0200

    Arithmatic class added  commit

--- fisrt_app/main/src/Arithmetics.java
+++ fisrt_app/main/src/Arithmetics.java
@@ -1,7 +1,19 @@
 public class Arithmetics {
+
+  private int x ; // private field x
+  private int y;  // private field y
+
+  public void setX(int x) {
+      this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
   // instance methods needs to be invoked by instance
-  public int sum(int x , int y){
-    return x+y;
+  public  int sum(){
+    return this.x+ this.y;
   }
 
 }
--- fisrt_app/main/src/Main.java
+++ fisrt_app/main/src/Main.java
@@ -3,6 +3,18 @@ public class Main {
   public static void main(String[] args) {
     // Main x = new Main(); // instantiation
     System.out.println(greeting("Abu Hamzah"));
+
+   Arithmetics obj = new Arithmetics();
+   Arithmetics obj2 = new Arithmetics();
+
+   obj.setX(5);
+   obj.setY(6);
+    int result =  obj.sum();
+
+   obj2.setX(25);
+   obj2.setY(50);
+    System.out.println(result);
+    System.out.println(obj2.sum());
   }
   // static method is a method can be invoked by the class
   // inside the class we can invoke it without anything
